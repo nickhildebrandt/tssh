a7fe2b346bff9bf42f06f60e6a9f79950148d689
Nick Hildebrandt <N.Hildebrandt@ts13.de>
2024-07-31 09:53:09 +0200

    Initial commit
    Add license, readme, and tssh.sh command Linux options structure with
    input filtering and checks.

---
+++ README.md
@@ -0,0 +1,112 @@
+# TSSH
+
+> TSSH is currently under development and not usable. For further information,
+> look in the dev branch.
+
+The TSSH project is a collection of Bash scripts designed to simplify the
+creation of a secure client-server ecosystem based on the current stable
+release of Debian GNU/Linux. The server functions as a router, firewall, print
+and file server with DHCP, DNS, TFTP, and SFTP capabilities. The client runs
+a streamlined version of the GNOME desktop environment, featuring core stable
+GTK4 applications and custom configuration defaults, to create an optimal Linux
+environment for development, multimedia, and office work.
+
+Data resilience and security are among the project's main goals. Both the
+client and server support Btrfs RAID 1 configuration, ensuring that all user
+data is securely synchronized over SFTP between clients and servers. This data
+remains accessible even when clients are outside the server's network and persist
+server OS reinstallations.
+
+The primary objective of the project is to automate and test the entire
+infrastructure, making it maintainable for an individual over the long term
+while minimizing dependencies.
+
+## Installation
+
+TSSH always runs on the latest stable release of Debian. The main script,
+tssh.sh, includes an install option that automatically copies all the required
+files and installs the necessary packages. The uninstall option can be used to
+remove all the installed files and packages.
+
+```shell
+sudo bash tssh.sh install
+```
+
+## Usage
+
+TSSH has the folowing options:
+
+### Install TSSH
+
+Install TSSH and dependencies to the system.
+
+```shell
+sudo tssh install
+```
+### Remove TSSH
+
+Remove TSSH from the system, but leave dependencies.
+
+```shell
+sudo tssh remove
+```
+
+### Setup Installmedia
+
+Create a live installation medium for server and clients.
+
+```shell
+sudo tssh setup-install \
+    -d <live medium e.g. /dev/sda or live.img> \
+    -k <root password for live login>
+```
+
+### Setup Server
+
+Install server role to the current machine.
+
+- TSSH will always build a Btrfs raid 1 if multiple drives a specified.
+- The second drive is always optional but recommended for data resilience.
+- The root password will become your master key that is needed to set up clients.
+- The -u and -p flags are only relevant when you use PPP as your wan connection
+  for MS-CHAP authentication.
+
+```shell
+sudo tssh setup-server \
+    -d <btrfs-pool-drive1 e.g. /dev/sda> [-d <btrfs-pool-drive2>] \
+    -k <root-password (Masterkey)> \
+    -w <network interface connect to the modem e.g. enp5s0> \
+    -m <dhcp | ppp> \
+    [-u <ppp username>] \
+    [-p <ppp password>]
+```
+
+### Setup Client
+
+Install client role on the current machine and pull user data and configuration
+from the server.
+
+- TSSH will always build a Btrfs raid 1 if multiple drives a specified.
+- The second drive is always optional but recommended for data resilience.
+- The username will be the first name in lowercase.
+- The first and last name, as well as the hostname, can only contain characters
+  A-Z and numbers 0-9, and must be a maximum of 30 characters long.
+- The root password is the (Masterkey) and needed to get the client
+  configuration form the server.
+
+```shell
+sudo tssh setup-client \
+    -d <btrfs-pool-drivei1 e.g. /dev/sda> [-d <btrfs-pool-drive2>] \
+    -h <Hostname>
+    -f <Firstname (machine owner name) e.g. Nick> \
+    -l <Lastname (machine owner name) e.g. Hildebrandt> \
+    -k <root-password (Masterkey)>
+```
+
+## Contributing
+
+I am very grateful for all feedback. A good piece of software can only be
+achieved when people test it and report errors and bugs. Feel free to create an
+issue on GitHub when you encounter problems, bugs, or errors. For code
+contributions, use the pull request feature on GitHub, and I will be thankful
+to include your work.
---
+++ tssh.sh
@@ -0,0 +1,332 @@
+#!/bin/bash
+#
+# Copyright 2024 Nick Hildebrandt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+action_ok()
+{
+    printf "[\e[32m OK \e[0m] %s\n" "${*}"
+}
+
+action_fail()
+{
+    printf "[\e[31m FL \e[0m] %s\n" "${*}" 1>&2
+    exit 1
+}
+
+update_apt()
+{
+    apt-get --assume-yes update
+    action_ok "Updated apt package index"
+}
+
+pkgi()
+{
+    apt-get --assume-yes \
+            --no-install-recommends \
+            --install-suggests \
+            install "${@}"
+}
+
+check_drives()
+{
+    for drive in "${@}"; do
+
+        if [[ "$(lsblk --nodeps \
+                       --noheadings \
+                       --output TYPE \
+                       "${drive}" 2> /dev/null)" != "disk" ]]; then
+            action_fail "'${drive}' is not a valid device"
+        fi
+
+        if [[ "$(blockdev --getsize64 "${drive}")" -lt 15000000000 ]]; then
+            action_fail "'${drive}' needs to be bigger than 20GB"
+        fi
+
+    done
+}
+
+check_string()
+{
+    for str in "${@}"; do
+
+        if ! [[ "${str}" =~ ^[a-zA-Z0-9]+$ &&
+                "${#str}" -le 30 ]]; then
+            action_fail "'${*}' is not valid"
+        fi
+
+    done
+}
+
+cp()
+{
+    command cp --verbose --recursive --update "${@}"
+}
+
+mkdir()
+{
+    command mkdir --verbose --parents "${@}"
+}
+
+rm()
+{
+    command rm --verbose --recursive --force "${@}"
+}
+
+install()
+{
+    command install --verbose "${@}"
+}
+
+ln()
+{
+    command ln --verbose --symbolic --force "${@}"
+}
+
+install_tssh()
+{
+    if [[ $# -gt 0 ]]; then
+        action_fail "install-tssh takes no arguments"
+    fi
+
+    mkdir "/opt/tssh/modules"
+    install "${TSSH_SH}" "/opt/tssh/tssh.sh"
+    cp "${TSSH_PATH}/modules" "/opt/tssh"
+    ln "/opt/tssh/tssh.sh" "/usr/bin/tssh"
+    action_ok "Installing files"
+
+    update_apt
+
+    pkgi iproute2 \
+         fdisk \
+         dosfstools \
+         btrfs-progs \
+         squashfs-tools \
+         debootstrap \
+         openssh-client \
+         grub-efi-amd64 \
+         ovmf \
+         qemu-system-x86 \
+         qemu-utils
+
+    action_ok "Installing dependencies"
+}
+
+remove_tssh()
+{
+    if [[ $# -gt 0 ]]; then
+        action_fail "remove-tssh takes no arguments"
+    fi
+
+    rm "/opt/tssh"
+    rm "/usr/bin/tssh"
+
+    action_ok "TSSH removed - apt packages will still be installed"
+}
+
+setup_server()
+{
+    while getopts d:k:w:m:u:p: opt
+    do
+        case "${opt}" in
+            d) DRIVES+=("${OPTARG}");;
+            k) ROOT_PW="${OPTARG}";;
+            w) WAN_LINK="${OPTARG}";;
+            m) WAN_MODE="${OPTARG}";;
+            u) WAN_USER="${OPTARG}";;
+            p) WAN_PW="${OPTARG}";;
+            *) action_fail "Run 'tssh help' for usage information";;
+        esac
+    done
+
+    if [[ -z "${DRIVES+x}" ||
+          -z "${ROOT_PW+x}" ||
+          -z "${WAN_LINK+x}" ||
+          -z "${WAN_MODE+x}" ]]; then
+        action_fail "Missing options - run 'tssh help' for usage information"
+    fi
+
+    check_drives "${DRIVES[@]}"
+
+    if ! [[ -d "/sys/class/net/${WAN_LINK}" ]]; then
+        action_fail "WAN interface is not valid"
+    fi
+
+    if ! [[ "${WAN_MODE}" == "dhcp" ||
+          "${WAN_MODE}" == "ppp" ]]; then
+        action_fail "-m (WAN mode) must be dhcp or ppp"
+    fi
+
+    TARGET_HOSTNAME=server
+
+    action_ok "Validated variables"
+}
+
+setup_client()
+{
+    while getopts d:h:f:l:k: opt
+    do
+        case "${opt}" in
+            d) DRIVES+=("${OPTARG}");;
+            h) TARGET_HOSTNAME="${OPTARG}";;
+            f) FIRST_NAME="${OPTARG}";;
+            l) LAST_NAME="${OPTARG}";;
+            k) ROOT_PW="${OPTARG}";;
+            *) action_fail "Run 'tssh help' for usage information";;
+        esac
+    done
+
+    if [[ -z "${DRIVES+x}" ||
+          -z "${TARGET_HOSTNAME+x}" ||
+          -z "${FIRST_NAME+x}" ||
+          -z "${LAST_NAME+x}" ||
+          -z "${ROOT_PW+x}" ]]; then
+        action_fail "Missing options - run 'tssh help' for usage information"
+    fi
+
+    check_drives "${DRIVES[@]}"
+    check_string "${TARGET_HOSTNAME}"
+    check_string "${FIRST_NAME}"
+    check_string "${LAST_NAME}"
+
+    action_ok "Validated variables"
+}
+
+setup_installer()
+{
+    while getopts d:k:t: opt
+    do
+        case "${opt}" in
+            d) DRIVES+=("${OPTARG}");;
+            k) ROOT_PW="${OPTARG}";;
+            *) action_fail "Run 'tssh help' for usage information";;
+        esac
+    done
+
+    if [[ -z "${DRIVES+x}" ||
+          -z "${ROOT_PW+x}" ]]; then
+        action_fail "Missing options - run 'tssh help' for usage information"
+    fi
+
+    check_drives "${DRIVES[@]}"
+    TARGET_HOSTNAME=live
+
+    action_ok "Validated variables"
+}
+
+deploy()
+{
+    if [[ $# -gt 0 ]]; then
+        action_fail "deploy takes no arguments"
+    fi
+}
+
+dev()
+{
+    if [[ $# -gt 0 ]]; then
+        action_fail "dev takes no arguments"
+    fi
+
+    if ! dpkg --status shellcheck > /dev/null; then
+        action_fail "'shellcheck' is not available"
+    fi
+
+    action_ok "'shellcheck' found"
+
+    shellcheck --check-sourced \
+               --external-sources \
+               --color \
+               --shell bash \
+               --enable=all \
+               "${TSSH_SH}"
+}
+
+help()
+{
+    if [[ $# -gt 0 ]]; then
+        action_fail "help takes no arguments"
+    fi
+
+    cat << EOF
+Usage: tssh.sh [COMMAND] [OPTIONS]
+
+Commands:
+
+  install             Install TSSH and its dependencies.
+
+  remove              Remove TSSH files, but leave dependencies installed.
+
+  setup-server        Setup the server configuration.
+                      Options:
+                        -d <drive>    Specify drive(s).
+                        -k <password> Set root password (Masterkey).
+                        -w <link>     Set WAN link.
+                        -m <mode>     Set WAN mode (dhcp | ppp).
+                        -u <user>     Set WAN user (only when using ppp).
+                        -p <password> Set WAN password (only when using ppp).
+
+  setup-client        Setup the client configuration.
+                      Options:
+                        -d <drive>    Specify drive(s).
+                        -h <hostname> client hostname.
+                        -f <fistname> client owner name.
+                        -l <lastname> client owner name.
+                        -k <password> Set root password (Masterkey).
+
+  setup-installer     Setup the installer configuration.
+                      Options:
+                        -d <drive>    Specify removeable drive(s) or file(s).
+                        -k <password> Set live root password.
+
+  deploy              Deploy the configuration. No additional options.
+
+  dev                 Run development checks. Requires 'shellcheck'.
+
+  help                Display this help message.
+EOF
+
+    exit 0
+}
+
+TSSH_SH="$(realpath "${0}")"
+TSSH_PATH="$(dirname "${TSSH_SH}")"
+
+if [[ "${UID}" -eq 0 ]]; then
+    action_ok "Root check"
+else
+    action_fail "Root check"
+fi
+
+if [[ $# -lt 1 ]]; then
+    action_fail "No argument"
+fi
+
+case "${1}" in
+    "install") shift; install_tssh "${@}";;
+    "remove") shift; remove_tssh "${@}";;
+    "setup-server") shift; setup_server "${@}";;
+    "setup-client") shift; setup_client "${@}";;
+    "setup-installer") shift; setup_installer "${@}";;
+    "deploy") shift; deploy "${@}";;
+    "dev") shift; dev "${@}";;
+    "help") shift; help "${@}";;
+    *) action_fail "Run 'tssh help' for usage information";;
+esac
